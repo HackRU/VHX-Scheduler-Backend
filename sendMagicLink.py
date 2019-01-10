cea9c2a0e9235e2e1826874e0fe0086bab8f936f
Sail338 <srihari.shankar1@gmail.com>
2019-01-10 12:40:36 -0500

    add magic link code

--- sendMagicLink.py
+++ sendMagicLink.py
@@ -1,7 +1,26 @@
 import config
+import requests
 
 def sendMagicLink(event,context):
     """
         Allows Directors to send magic links to people with X permission
     """
-    pass
+    if 'email' not in event or 'token' not in event or 'emailsToSend' not in event or 'permissions' not in event:
+        ret = {"statusCode":400, "body":"Invalid input"}
+        return config.add_cors_headers(ret)
+    #extract out the email, auth token and just forward to the lcs api
+    auth_email = event['email']
+    #build the lists to send the magic links
+    emails_unparsed = event['emailsToSend']
+    #parse out comma seperated list if any
+    emails_parsed = emails_unparsed.split(",")
+    permissions = event['permissions'].split(",")
+    request_info = {
+        "email":auth_email,
+        "token":event['token'],
+        "emailsTo":emails_parsed,
+        "permissions":permissions
+    }
+    responseFromRequest = requests.post(config.BASE_URL + "/magiclink", json = (request_info))
+    return responseFromRequest.json()
+
