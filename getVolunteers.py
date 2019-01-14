58d23fd508e25e9f26862170c7b83c79c448795d
Sail338 <srihari.shankar1@gmail.com>
2019-01-14 11:49:07 -0500

    add insert action

--- getVolunteers.py
+++ getVolunteers.py
@@ -17,6 +17,24 @@ def getVolunteers(event,context):
             resp = table.query(
                 KeyConditionExpression=Key('email').eq(resp.body[i]['email'])
             )
+            print(resp)
             if resp:
                 response[i]['current_action'] = resp['current_action']
     return response
+
+def insertVolunteerAction(event,context):
+    """
+        Inserts a current volunteer action into the DB
+    """
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    tableToCheck = dynamodb.Table('VolunteerActions')
+    tableToCheck.update_item(
+        Key={
+            'email': event['user_email']
+        },
+        UpdateExpression="set current_action = :val",
+        ExpressionAttributeValues={
+            ':val': event['updated_val']
+         }
+    ) 
+
