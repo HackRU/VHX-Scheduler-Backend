8ff33ec061afc3a4b3bd2800f1c2159dfa2a320b
Sail338 <srihari.shankar1@gmail.com>
2019-01-14 10:45:09 -0500

    query dynamo table

--- getVolunteers.py
+++ getVolunteers.py
@@ -1,5 +1,6 @@
 import requests
 import config
+import boto3
 def getVolunteers(event,context):
 
     """
@@ -8,4 +9,14 @@ def getVolunteers(event,context):
 
     profile_request_data_dict = {"email":event["email"],"token":event["token"], "query":{"role.volunteer":True}}
     dat = requests.post(config.BASE_URL + "/read", json = (profile_request_data_dict))
-    return dat.json()
+    response = dat.json()
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    tableToCheck = dynamodb.Table('VolunteerActions')
+    if response.statusCode == 200:
+        for i in range(len(response.body()))
+            resp = table.query(
+                KeyConditionExpression=Key('email').eq(resp.body[i]['email'])
+            )
+            if resp:
+                response[i]['current_action'] = resp['current_action']
+    return response
